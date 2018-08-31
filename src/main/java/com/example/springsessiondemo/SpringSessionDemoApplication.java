b42ddff456e4e86f8876ee1390ed9fc0be5c745b
H299633 <shuzhen.ye@honeywell.com>
2018-08-31 15:44:30 +0800

    create git

---
+++ src/main/java/com/example/springsessiondemo/SpringSessionDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsessiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (SpringSessionDemoApplication.class,args);
+    }
+}
---
+++ src/main/java/com/example/springsessiondemo/controller/CookieController.java
@@ -0,0 +1,47 @@
+package com.example.springsessiondemo.controller;
+
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author: Kayla,Ye
+ * @Description:
+ * @Date:Created in 2:40 PM 8/31/2018
+ */
+@RestController
+public class CookieController
+{
+    @RequestMapping("/test/cookie")
+    public  String cookie(@RequestParam("browser") String  browser,HttpServletRequest request,HttpSession session){
+
+
+        /**
+         * 1 、取出session中的browser
+         * 2、如果没有则将request 中的borwser 存入session
+         * 3、如果获取到session ,则将request 中的browser 输出
+         * 4、 打印所有的cookie
+         **/
+        Object sessionBrower = session.getAttribute ("browser");
+        if( sessionBrower == null ){
+            System.out.println ("不存在session, 设置browser=" + browser);
+            session.setAttribute ("browser", browser);
+        }else {
+            System.out.println ("存在session, browser="+ sessionBrower.toString ());
+        }
+
+        Cookie[] cookies = request.getCookies ();
+        if (cookies != null && cookies.length > 0){
+            for (Cookie cookie: cookies){
+                System.out.println (cookie.getName ()+":"+cookie.getValue ());
+            }
+        }
+
+        return  "index";
+    }
+}
