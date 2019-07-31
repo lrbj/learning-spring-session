6b8db15df67848da253f4e59c9b58a4614c4d8da
H299633 <shuzhen.ye@honeywell.com>
2019-07-31 16:06:56 +0800

    fix：添加redis数据库池

--- src/main/java/com/example/springsessiondemo/SpringSessionDemoApplication.java
+++ src/main/java/com/example/springsessiondemo/SpringSessionDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.springsessiondemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringSessionDemoApplication {
